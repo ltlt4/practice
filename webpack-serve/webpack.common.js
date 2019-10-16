8a92ff66b500699a52c156dd5a17e6f089ac5b6c
ltlt4 <403097929@qq.com>
2019-10-16 11:26:56 +0800

    cheshi

--- webpack-serve/webpack.common.js
+++ webpack-serve/webpack.common.js
@@ -3,17 +3,17 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        index: './src/js/index.js'
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Production'
-        })
-    ],
-    output: {
-        filename: '[name].min.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  entry: {
+    index: './src/js/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Production'
+    })
+  ],
+  output: {
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
